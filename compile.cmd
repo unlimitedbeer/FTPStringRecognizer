58ccfabe3048f5226242dc723f4e846e0a8f4928
unlimitedbeer <unlimitedbeer@mail.ru>
2015-03-16 16:51:29 +0300

    Version 1.0.1
    Added recognizer.sm
    Added compile.cmd

---
+++ compile.cmd
@@ -0,0 +1,4 @@
+del .\recognizer_sm.cpp
+del .\Recognizer\recognizer_sm.h
+java -jar D:\download\coding\smc\smc_6_5_0\bin\Smc.jar -c++ -d recognizer recognizer.sm
+move .\Recognizer\recognizer_sm.cpp .\recognizer_sm.cpp
